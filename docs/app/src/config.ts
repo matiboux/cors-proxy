34faab8f67790a8fba6ddb8fb998c88dcff2fa13
Matiboux <matiboux@gmail.com>
2025-01-22 00:31:25 +0100

    Configure i18n localeKeys

--- docs/app/src/config.ts
+++ docs/app/src/config.ts
@@ -1,10 +1,12 @@
 import type { AstroConfig } from 'astro'
 
+import en from './locales/en'
+import fr from './locales/fr'
+
 type LocaleKeys = Record<string, Record<string, string>>
 type I18nConfig = AstroConfig['i18n'] & { localeKeys?: LocaleKeys }
 
-export const i18n: I18nConfig =
-{
+export const i18n: I18nConfig = {
 	locales: [
 		{
 			codes: ['en', 'en_US'],
@@ -19,7 +21,7 @@ export const i18n: I18nConfig =
 	fallback: {
 		fr: 'en',
 	},
-	// localeKeys: {},
+	localeKeys: { en, fr },
 	routing: {
 		prefixDefaultLocale: false,
 		redirectToDefaultLocale: true,
